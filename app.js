d34431c09da9d629dc4eaa844dfe801290aae36f
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2024-02-03 16:48:30 +0530

    minor fix

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
+function auth(req, res, next) {
+  const apiKey = req.headers['x-api-key'];
+  if (apiKey === process.env.API_KEY) {
+    next();
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+}
+
+app.use('/', auth, indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
